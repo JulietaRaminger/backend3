c21c7ab122b0ae2a1a0c527b879d9f2132b407e5
Julieta Raminger <julieta.raminger@gmail.com>
2024-10-10 18:10:48 -0300

    Arreglos entrega Backend3

--- src/app.js
+++ src/app.js
@@ -9,15 +9,15 @@ import { errorHandle } from "./errors/errHandle.js";
 const app = express();
 const PORT = process.env.PORT || 8080;
 const connection = mongoose.connect(
-    `mongodb+srv://julietaraminger:ImIimWsRYLs3oThf@cluster0.uwq4ups.mongodb.net/Adoptame`
-  );
+  `mongodb+srv://julietaraminger:ImIimWsRYLs3oThf@cluster0.uwq4ups.mongodb.net/Adoptame`
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", router);
 
-// Middleware de manejo de errores
+//middleware de manejo de errores
 app.use(errorHandle);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
