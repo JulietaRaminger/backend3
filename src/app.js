713c1bd59442b11755cd5ec36d4c7f8875e1845e
Julieta Raminger <julieta.raminger@gmail.com>
2024-10-10 19:03:24 -0300

    arreglando app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-
+import dotenv from "dotenv"; 
 import router from "./routes/index.js";
-
 import { errorHandle } from "./errors/errHandle.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Configura la opción strictQuery
+mongoose.set('strictQuery', true); // O false, según tu preferencia
+
 const connection = mongoose.connect(
-  `mongodb+srv://julietaraminger:ImIimWsRYLs3oThf@cluster0.uwq4ups.mongodb.net/Adoptame`
+  process.env.MONGO_URI // Usando la variable de entorno
 );
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use("/api", router);
 
-//middleware de manejo de errores
+// Middleware de manejo de errores
 app.use(errorHandle);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
